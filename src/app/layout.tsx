0fb12b942b3c39da0f488a7bdc50783572a356aa
angelitasolis <mariangel.solis.bils@gmail.com>
2025-06-05 18:03:50 -0600

    login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,18 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// src/app/layout.tsx
+import './globals.css'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Bank JAM",
-  description: "Bank JAM, Jordan-Mariangel-Allan"
-};
+  title: 'BANCO JAM',
+  description: 'Simulación SINPE 2025',
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="es">
+      <body className="bg-black text-white">
         {children}
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-import Image from "next/image";
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex items-center space-x-4">
-        <h1 className="text-3xl font-bold underline text-center">
-          Banco JAM, como mermelada — Jordan, Mariangel y Allan
-        </h1>
-        <Image
-          src="/jam.png"
-          alt="Logo JAM"
-          width={50}
-          height={50}
-        />
-      </div>
-    </div>
-  );
+  redirect('/login')
 }
